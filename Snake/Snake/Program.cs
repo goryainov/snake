85703b928e1bfd3cf68d5d4f5eb200c01488d03a
goryainov <goryainov.vn@gmail.com>
2017-10-19 22:02:08 +0300

    Создан класс Snake и Direction, внесены изменения в класс Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -22,7 +22,8 @@ namespace Snake
             rightLine.Draw();
 
             Point p = new Point(4, 5, '*');
-            p.Draw();
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
         }
     }
 }
