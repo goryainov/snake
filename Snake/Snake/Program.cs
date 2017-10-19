5ffb4bad64b23cce55de8d024f73036df0f5c10e
goryainov <goryainov.vn@gmail.com>
2017-10-19 09:37:29 +0300

    Добавлена рамка для игры

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,11 +16,14 @@ namespace Snake
 			Point p2 = new Point(4, 5, '#');
 			p2.Draw();
 
-			HorizontalLine hLine = new HorizontalLine(5, 10, 8, '+');
-			hLine.Draw();
-
-			VerticalLine vLine = new VerticalLine(5, 10, 20, '|');
-			vLine.Draw();
+			HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+			VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
 			Console.ReadLine();
         }
